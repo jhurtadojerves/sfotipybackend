bd8d12f545baea44d7a130f891fa55c32cd5d38d
Juliens-Hurtado <juliens@outlook.com>
2014-09-25 11:37:30 -0500

    Subir antes de los API Rest

--- sfotipy/artists/views.py
+++ sfotipy/artists/views.py
@@ -5,9 +5,10 @@ from django.views.generic.detail import DetailView
 
 from .models import Artist
 
-class ArtistDetailView(DetailView):
-	model = Artist
-	context_object_name = 'artist'
 
-	def get_template_names(self):
-		return 'artist.html'
+class ArtistDetailView(DetailView):
+    model = Artist
+    context_object_name = 'artist'
+
+    def get_template_names(self):
+        return 'artist.html'
